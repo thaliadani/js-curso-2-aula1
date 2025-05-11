14e46301832f4212256569846f945068b6ef2785
thaliadani <thaliadani2@gmail.com>
2025-05-11 18:16:27 -0300

    adiciona estrutura inicial do jogo do número secreto com HTML, CSS e JavaScript

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector("h1");
+titulo.innerHTML = "Jogo do número secreto";
+
+let paragrafo = document.querySelector("p");
+paragrafo.innerHTML = "Tente adivinhar o número secreto entre 1 e 10.";
+
+function verificarChute(){
+    
+}
